f7c103d1acfbe5736a13ce03d8d48eb67674b189
Ghazy01-Dev <ebrahema@std.mans.edu.eg>
2023-12-22 11:01:28 +0200

    ff

---
+++ database/migrations/2023_09_19_103331_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id')->index('posts_user_id_foreign');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
